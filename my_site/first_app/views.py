15a7f0713091b7a4fc3f61c68545ae57563f9488
Andriy Gula <gula@pr.km.ua>
2023-06-22 22:57:33 +0300

    url names

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+
 
 articles = {
     'sports': 'Sports Page',
@@ -9,8 +11,16 @@ articles = {
 
 
 def news_view(request, topic):
-    return HttpResponse(articles[topic])
+    try:
+        return HttpResponse(articles[topic])
+    except:
+        raise Http404()
 
 def add_view(request, num1, num2):
     return HttpResponse(f'{num1} + {num2} = {str(num1+num2)}')
 
+def num_page_view(request, num_page):
+    topics_list = list(articles.keys())
+    topic = topics_list[num_page]
+
+    return HttpResponseRedirect(reverse('topic-page', args=[topic]))
