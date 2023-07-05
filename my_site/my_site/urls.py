4d7c27ea5c9ba5241bcda48f61a093d085b3c3fd
Андрій Гула <gula@pr.km.ua>
2023-07-05 08:00:04 +0300

    Variables (again)

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('first_app/', include('first_app.urls')),
+    path('my_app/', include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
