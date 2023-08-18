824a064b546d00b5253c3b18c36db087824902b8
gulaa <gula@pr.km.ua>
2023-08-18 16:05:04 +0300

    Model and website p.2

--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import models
+from django.urls import reverse
 
 # Create your views here.
 def list(request):
-    return render(request, 'cars/list.html')
+    all_cars = models.Car.objects.all()
+    contex = {'all_cars':all_cars}
+    return render(request, 'cars/list.html', context=contex)
 
 def add(request):
-    return render(request, 'cars/add.html')
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Car.objects.create(brand=brand, year=year)
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 def delete(request):
-    return render(request, 'cars/delete.html')
+    if request.POST:
+        pk = request.POST['pk']
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('PK not found!')
+            return redirect(reverse('cars:list'))
+
+    else:
+        return render(request, 'cars/delete.html')
