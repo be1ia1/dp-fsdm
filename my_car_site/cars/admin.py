a35cd2a2d3c660d15364350ceb1e7c07dbecca4b
gulaa <gula@pr.km.ua>
2023-08-21 14:12:30 +0300

    Django admin and models

--- my_car_site/cars/admin.py
+++ my_car_site/cars/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from cars.models import Car
+
 
 # Register your models here.
+# default
+# admin.site.register(Car)
+
+# costumize admin
+class CarAdmin(admin.ModelAdmin):
+    # fields = ['year', 'brand']
+    fieldsets = [
+        ('TIME INFORMATION', {'fields': ['year']}),
+        ('CAR INFORMATION', {'fields': ['brand']})
+    ]
+
+admin.site.register(Car,CarAdmin)
