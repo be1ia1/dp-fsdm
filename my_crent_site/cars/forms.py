8e9f4866f39cbba643c5f5f598ac0e261c822252
Andriy Gula <gula@pr.km.ua>
2023-08-27 21:47:06 +0300

    Widgets and Styling

--- my_crent_site/cars/forms.py
+++ my_crent_site/cars/forms.py
@@ -5,4 +5,9 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=30, required=False)
     last_name = forms.CharField(label='Last Name', max_length=30, required=False)
     email = forms.EmailField(label='Email', required=False)
-    review = forms.CharField(label='Please write your review here', max_length=100, required=False)
+    review = forms.CharField(label='Please write your review here', 
+                             max_length=100, 
+                             required=False, 
+                             widget=forms.Textarea(attrs={'class':'myform',
+                                                          'rows':'2',
+                                                          'cols':'2'}))
