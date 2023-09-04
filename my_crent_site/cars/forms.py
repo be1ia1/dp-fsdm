ffc44ff11a6a0b9c5c6e706e22c0616146aa8098
Andriy Gula <gula@pr.km.ua>
2023-09-04 07:57:34 +0300

    ModelForms Customization

--- my_crent_site/cars/forms.py
+++ my_crent_site/cars/forms.py
@@ -1,13 +1,31 @@
 from django import forms
+from .models import Review
+from django.forms import ModelForm
 
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label='First Name', max_length=30, required=False)
+#     last_name = forms.CharField(label='Last Name', max_length=30, required=False)
+#     email = forms.EmailField(label='Email', required=False)
+#     review = forms.CharField(label='Please write your review here', 
+#                              max_length=100, 
+#                              required=False, 
+#                              widget=forms.Textarea(attrs={'class':'myform',
+#                                                           'rows':'2',
+#                                                           'cols':'2'}))
 
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=30, required=False)
-    last_name = forms.CharField(label='Last Name', max_length=30, required=False)
-    email = forms.EmailField(label='Email', required=False)
-    review = forms.CharField(label='Please write your review here', 
-                             max_length=100, 
-                             required=False, 
-                             widget=forms.Textarea(attrs={'class':'myform',
-                                                          'rows':'2',
-                                                          'cols':'2'}))
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        # fields = ['first_name', 'last_name', 'stars']
+        fields = "__all__"
+        labels = {
+            'first_name': 'YOUR FIRST NAME',
+            'last_name': 'LAST NAME',
+            'stars': 'Rating'
+        }
+        error_messages = {
+            'stars': {
+                'min_value': 'Yo! Min value is 1',
+                'max_value': 'Yo! Yo! Max value is 5'
+            }
+        }
